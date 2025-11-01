5410e6bc8b6a9bcdc3405ae674edd988258179c7
BenSin33 <daylacaitaikhoan13@gmail.com>
2025-11-01 22:40:31 +0700

    first commit

--- FloginFE_BE/backend/src/test/java/com/flogin/backend/BackendApplicationTests.java
+++ FloginFE_BE/backend/src/test/java/com/flogin/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.back_end;
+package com.flogin.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
