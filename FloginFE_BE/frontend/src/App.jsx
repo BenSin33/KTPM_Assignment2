5028865e466bfb9135f3c698178ad47003008155
BenSin33 <daylacaitaikhoan13@gmail.com>
2025-11-08 22:46:53 +0700

    đã hoàn thành unit test cho frontend Product

--- FloginFE_BE/frontend/src/App.jsx
+++ FloginFE_BE/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import ProductPage from "./component/Product";
+import ProductPage from "./component/product_dashboard/Product.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
