b4bcdc7801e2e144e31bdc7c8fbd56250f1d31d7
nguyenlequananh <lepodz2503@gmail.com>
2025-12-09 22:46:27 +0700

    Update AuthController.java

--- FloginFE_BE/backend/src/main/java/com/flogin/controller/AuthController.java
+++ FloginFE_BE/backend/src/main/java/com/flogin/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.flogin.entity.Account;
 import com.flogin.service.AccountService;
 import com.flogin.util.JwtUtil;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
@@ -16,40 +15,18 @@ import java.util.HashMap;
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
+    
 
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
 
+    public AuthController(AccountService accountService) {
+        this.accountService = accountService;
+    }
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> loginRequest) {
         String username = loginRequest.get("username");
         String password = loginRequest.get("password");
         Map<String, Object> response = new HashMap<>();
-
-        if (username == null || username.trim().isEmpty()) {
-            response.put("message", "Username is required");
-            response.put("success",false);
-            return ResponseEntity.badRequest().body(response);
-        }
-
-        if (username.length() < 3) {
-            response.put("message", "Username must be longer than 3 characters");
-            response.put("success",false);
-            return ResponseEntity.badRequest().body(response);
-        }
-
-        if (password == null || password.trim().isEmpty()) {
-            response.put("message", "Password is required");
-            response.put("success",false);
-            return ResponseEntity.badRequest().body(response);
-        }
-
-        if (password.length() < 6) {
-            response.put("message", "Password must be at least 6 characters");
-            response.put("success",false);
-            return ResponseEntity.badRequest().body(response);
-        }
-
         try {
             Account account = accountService.login(username, password);
             response.put("message", "Login successful");
@@ -61,7 +38,11 @@ public class AuthController {
         } catch (IllegalArgumentException e) {
             response.put("message", e.getMessage());
             response.put("success",false);
-            return ResponseEntity.badRequest().body(response);
+            if(e.getMessage().contains("Invalid username or password")||e.getMessage().contains("Incorrect password")) {
+                return ResponseEntity.status(401).body(response);
+            } else {
+                 return ResponseEntity.badRequest().body(response);
+            }
         }
     }
 }
